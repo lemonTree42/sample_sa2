50208d7a3ecd6ae8fc4d358b64f861e58d760a7f
Joel Hirzel <joel.hirzel@ost.ch>
2021-11-15 22:54:57 +0100

    init

---
+++ src/p2/Hotel2.java
@@ -0,0 +1,15 @@
+public class Hotel2 {
+    private int capacity;
+
+    public Hotel(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void bookRoom(int amount) {
+        capacity -= amount;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+}
